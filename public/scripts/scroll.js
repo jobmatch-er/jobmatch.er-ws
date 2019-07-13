2b8934f3ed89ad7b404f8cc6ba6bad635a558c7d
Jakob-Niklas See <blendercraft.info@gmail.com>
2019-07-13 19:34:44 +0200

    reshow login field, minor changes

---
+++ public/scripts/scroll.js
@@ -0,0 +1,15 @@
+
+function addParallaxScroll() 
+{
+    window.addEventListener('scroll', () => 
+    {
+        let parent = document.getElementsByClassName('jobmatcher-background').item(0);
+        let children = parent.getElementsByTagName('div');
+        for (let i = 0; i < children.length; i++) 
+        {
+            children[i].style.transform = 'translateY(-' + (window.pageYOffset * i / children.length) + 'px)';
+        }
+    }, false)
+
+    console.log("parallax");
+}
