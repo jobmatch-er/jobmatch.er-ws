3ba49b3e91ca67e5deca9d04567d1ce7e4d8f124
Jakob-Niklas See <blendercraft.info@gmail.com>
2019-07-15 17:31:12 +0200

    added mediaquerys for register inputs

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,20 +1,24 @@
 console.log("fddsfgdssdg")
-$(document).ready(function() {
-
-    $("#dropdownlol").change(function() {
+$(document).ready(function() 
+{
+    $("#dropdownlol").change(function() 
+    {
         console.log("ggggg")
-      var el = $(this) ;
-      if(el.val() === "Arbeitgeber" ) {
-      $("#optional").append(
-          "<div class='custominput'>\
-          <input type='text' name='city' placeholder='Skkjtadt' />\
-          <div class='accentbar'></div>\
-          </div>\
-          <br>\
-          <br>\
-          ");
-      } else if(el.val() === "Arbeitsuchender" ) {
-          $("#optional").remove() ; }
+        var el = $(this);
+        if (el.val() === "Arbeitgeber") 
+        {
+            $("#optional").append("\
+                <div class='custominput'>\
+                    <input type='text' name='city' placeholder='Skkjtadt' />\
+                    <div class='accentbar'></div>\
+                </div>\
+                <br>\
+                <br>\
+            ");
+        } 
+        else if (el.val() === "Arbeitsuchender") 
+        {
+            $("#optional").remove();
+        }
     });
-  
-  });
+});
